875f7e7ad68ecb0a95917350719f7c97953ced3c
HalilCan <halilcanmemoglu@gmail.com>
2020-08-03 14:32:41 +0300

    testing static rule applier

--- customStyleApplier.js
+++ customStyleApplier.js
@@ -1,19 +1,64 @@
 // TODO: host rules online
 
+const __debugMode = 1;
+
 let rulesFile = {
 	"rules": {
 		"www.google.com" : {
-			"body" : "border: 1px solid red",
-			".container" : "background-color: blue"
+			"content": {
+				"body" : "border: 5px solid red",
+				".hp" : "background-color: blue",
+				"#searchform" : "background-color: black"
+			},
+			"information": {
+				"author": "",
+				"creationDate": "",
+				"updateDate": "",
+				"votes": "0"
+			}
 		}
 	}
-};
-
-let rules = rulesFile['rules'];
-
-const hostName = window.location.hostName;
+}
 
 
-if (hostName in rules) {
-	
+let rules = rulesFile["rules"];
+
+const hostname = window.location.hostname;
+if (__debugMode) {
+// 	console.log(`window.location:`);
+// 	console.log(window.location);
+	console.log(`hostname: ${hostname}`);
+}
+
+if (hostname in rules) {
+	if (__debugMode) {
+		console.log(`${hostname} found in rules`);
+	}
+	// I  am considering making a more granular process on a 
+	// per-element basis, but that would take more time and browsers 
+	// manage additional CSS rules well anyway. (or even with 
+	// individual rule-pieces, but a similar argument applies)
+
+	let style = document.createElement("customStylesheet");
+	document.head.appendChild(style);
+	let sheet = style.sheet;
+
+	let cssString = "";
+
+	for ([key, rule] in rules) {
+		cssString += `${key} {\n 
+			${rule}\n
+		}\n\n`;
+	}
+
+	if (__debugMode) {
+		console.log(`expanded new css rules: ${cssString}`);
+		console.log(`sheet before: ${sheet}`);
+	}
+
+	sheet += cssString;
+
+	if (__debugMode) {
+		console.log(`sheet after: ${sheet}`);
+	}
 }
